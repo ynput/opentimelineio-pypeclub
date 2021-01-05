48ed8b263c8eaec57eba4d7fc0c0fec40bc43909
Jakub Jezek <jakub@orbi.tools>
2021-01-05 15:21:17 +0100

    initial state of plugins wip

--- opentimelineio_pypeclub/application_plugins/davinci_resolve/__init__.py
+++ opentimelineio_pypeclub/application_plugins/davinci_resolve/__init__.py
@@ -1,23 +1,5 @@
-# TODO: some of functions can probably by a media linker
-# TODO: open time have probably some time op functions
-
 import re
 import opentimelineio as otio
-
-
-def timecode_to_frames(timecode, framerate):
-    rt = otio.opentime.from_timecode(timecode, 24)
-    return int(otio.opentime.to_frames(rt))
-
-
-def frames_to_timecode(frames, framerate):
-    rt = otio.opentime.from_frames(frames, framerate)
-    return otio.opentime.to_timecode(rt)
-
-
-def frames_to_secons(frames, framerate):
-    rt = otio.opentime.from_frames(frames, framerate)
-    return otio.opentime.to_seconds(rt)
 
 
 def get_reformated_path(path, padded=True):
@@ -64,3 +46,18 @@ def get_padding_from_path(path):
         return len(re.findall(padding_pattern, path).pop())
 
     return None
+
+
+def timecode_to_frames(timecode, framerate):
+    rt = otio.opentime.from_timecode(timecode, framerate)
+    return int(otio.opentime.to_frames(rt))
+
+
+def frames_to_timecode(frames, framerate):
+    rt = otio.opentime.from_frames(frames, framerate)
+    return otio.opentime.to_timecode(rt)
+
+
+def frames_to_secons(frames, framerate):
+    rt = otio.opentime.from_frames(frames, framerate)
+    return otio.opentime.to_seconds(rt)
